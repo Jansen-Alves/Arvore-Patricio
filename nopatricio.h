cfb8645ad92c4fb0344304f3e9ed79d5459cb09c
Jansen-Alves <75702771+Jansen-Alves@users.noreply.github.com>
2023-12-12 08:52:10 -0300

    Update nopatricio.h

--- nopatricio.h
+++ nopatricio.h
@@ -6,20 +6,13 @@
 
 NO *criarNOh(char * chave, int reg, NO*pai, NO*esquerdo, NO*direito, int val){
     NO *novo = (NO *)malloc(sizeof(NO));
-    printf("Memoria alocada");
+    printf("Memoria alocada\n");
     strcpy(novo->chave, chave);
-        printf("nome alocado");
+        printf("nome alocado\n");
         novo->reg = reg;
-        if(val == -1){
-            novo->pai = NULL;
-            novo->esq = NULL;
-            novo->dir = NULL;
-        }
-        else{
-            novo->pai = pai;
-            novo->esq = esquerdo;
-            novo->dir = direito;
-        }
+        novo->pai = NULL;
+        novo->esq = NULL;
+        novo->dir = NULL;
 
         novo->folha = val;
         printf("\n> Novo cliente criado\n");
@@ -63,29 +56,31 @@ void ajeitarArv(NO* folha){
                         val = 1;
                     }
                 }else{
-                    val = 1;
+                    val = 2;
                 }
             }
             //Processo de liberar da memória os nós descartados do ziguezague
             sweep = folha->pai;
             v1 = sweep->pai;
-            while(k>0){
+            while(k>0 && val == 1){
                 sweep= v1;
                 v1 = sweep->pai;
                 if(k == 1){
-                    if(v1->dir == sweep){
+
+                    /*if(v1->dir == sweep){
                         lado = 1;
                     }
                     else if(v1->esq == sweep){
                         lado = -1;
-                    }
+                    }*/
                 }
                 free(sweep);
                 k--;
                 }
 
            z->pai = v1;
-            if(lado >0){
+           lado = v1->reg;
+            if(z->chave[lado]>0){
                 v1->dir = z;
             }else{
                 v1->esq = z;
@@ -150,6 +145,9 @@ int inserirpat(NO* arv, char* x, int n){
   NO* yf;
   NO* pr;
   pr = buscapat(arv, x, n);
+
+  l = 0;
+  c = 0;
   if(pr->chave == x){
     printf("Inserção invalida, chave já existente\n");
     return -1;
@@ -157,6 +155,7 @@ int inserirpat(NO* arv, char* x, int n){
   else{
     printf("chave não existente na arvore\n");
   }
+
   printf("A chave tem folha igual ah %d \n", pr->folha);
   if(pr->folha == 0){
     printf("busca acabou em nó interno\n");
@@ -180,41 +179,53 @@ int inserirpat(NO* arv, char* x, int n){
     if(yf->folha == 1){
         c = strlen(yf->chave);
         l = maiorprefixo(yf->chave, x);
-    if(l == strlen(x) || l == c){
-        printf("Inserção inválida. Uma chave é prefixa de outra");
-        return -1;
-    }
-    z = yf->pai;
-    if(z->reg> l+1){
-        while(z->pai->reg > l+1){
+        if(l == strlen(x) || l == c){
+            printf("Inserção inválida. Uma chave é prefixa de outra");
+            return -1;
+        }
+        z = yf->pai;
+        if(z->reg > l+1){
+            while(z->pai->reg > l+1){
             z = z->pai;
           }
-    }else{
-        z = yf;
-    }
-    }else if(pr->folha == - 1){
-        printf("só há raiz da arvore");
-        z = yf;
-    }
+        }else{
+            z = yf;
+        }
+        }else if(pr->folha == - 1){
+            printf("só há raiz da arvore \n");
+            z = yf;
+        }
     reg_w = atoi(x);
     w = criarNOh(x, reg_w, NULL,NULL,NULL, 1);
-    if(z->folha > 0){
+    printf("criando noh w");
+    if(z->folha >= 0){
         if(x[l+1] == 0){
           v = criarNOh(NULL, l+1, z->pai, w, z, 0);
+          v->pai = z->pai;
+          v->esq = w;
+          v->dir = z;
         }else{
           v = criarNOh(NULL, l+1, z->pai, z, w, 0);
+          v->pai = z->pai;
+          v->esq = z;
+          v->dir = w;
         }
         z->pai = v;
         w->pai = v;
     }else{
+        printf("criando filhos da raiz\n");
         if(x[l+1] == 0){
-          v = criarNOh(NULL, l+1, z->pai, w, NULL, -1);
+          v = criarNOh("null", l+1, NULL, w, NULL, -1);
+          v->esq = w;
         }else{
-          v = criarNOh(NULL, l+1, z->pai, NULL, w, -1);
+          v = criarNOh("null", l+1, NULL, NULL, w, -1);
+          v->dir = w;
     }
+        printf("Noh v criado\n");
         arv = v;
         free(z);
     }
+    printf("terminei a alocação");
     ajeitarArv(w);
     printf("chave criada");
 }
